dd596d499f5414a1edda9becd7d98ccb276d8db5
Anushree Anandani <aa11093@nyu.edu>
2025-07-21 13:32:01 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
 
-import netlify from "@astrojs/netlify";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://anushreeanandani.github.io',
-  output:'static',
+  base: '/',
+  output: 'static',
   integrations: [mdx(), sitemap(), tailwind(), icon()],
-  adapter: netlify(),
 });
